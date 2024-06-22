273c58fd0357e3378f26acc5a4fe1621371be4bd
yusuf <yusufgad24@gmail.com>
2024-06-22 17:55:00 +0300

    Add auth

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,21 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-6 text-xl font-semibold">
+      <div>Gallery</div>
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
+
+export default TopNav;
