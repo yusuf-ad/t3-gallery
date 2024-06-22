20b670d73e2f6173ee13ebe4f1fa9475608a1c4b
yusuf <yusufgad24@gmail.com>
2024-06-22 15:19:42 +0300

    Force dynamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
   "https://utfs.io/f/ce5542ed-ae4e-497e-a399-a9c36de7b11f-sezbjl.png",
   "https://utfs.io/f/a182376a-4662-478c-b10e-6c3a220dd782-tehexf.png",
