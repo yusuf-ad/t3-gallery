1cb6d70b541bdce5794b698c8af0256ea551803c
yusuf <yusufgad24@gmail.com>
2024-06-24 17:40:07 +0300

    Basic posthog analytics

---
+++ src/app/_analytics/provider.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useAuth, useUser } from "@clerk/nextjs";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useEffect, type ReactNode } from "react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: "/ingest",
+    ui_host: "https://app.posthog.com",
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+  });
+}
+export function CSPostHogProvider({ children }: { children: ReactNode }) {
+  return (
+    <PostHogProvider client={posthog}>
+      <PostHogAuthWrapper>{children}</PostHogAuthWrapper>
+    </PostHogProvider>
+  );
+}
+
+function PostHogAuthWrapper({ children }: { children: ReactNode }) {
+  const auth = useAuth();
+  const userInfo = useUser();
+
+  useEffect(() => {
+    if (userInfo.user) {
+      posthog.identify(userInfo.user.id, {
+        email: userInfo.user.emailAddresses[0].email,
+        name: userInfo.user.fullName,
+      });
+    } else if (!auth.isSignedIn) {
+      posthog.reset();
+    }
+  }, [userInfo, auth]);
+
+  return children;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
 import { Toaster } from "sonner";
+import { CSPostHogProvider } from "./_analytics/provider";
 
 export const metadata = {
   title: "T3 Gallery",
@@ -24,27 +25,29 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <NextSSRPlugin
-        /**
-         * The `extractRouterConfig` will extract **only** the route configs
-         * from the router to prevent additional information from being
-         * leaked to the client. The data passed to the client is the same
-         * as if you were to fetch `/api/uploadthing` directly.
-         */
-        routerConfig={extractRouterConfig(ourFileRouter)}
-      />
-      <html lang="en" className={`${GeistSans.variable}`}>
-        <body className="dark">
-          <Toaster />
-          <div className="grid h-screen grid-rows-[auto,1fr]">
-            <TopNav />
-            <main className="overflow-y-auto">{children}</main>
-            {modal}
-          </div>
+      <CSPostHogProvider>
+        <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
+        <html lang="en" className={`${GeistSans.variable}`}>
+          <body className="dark">
+            <Toaster />
+            <div className="grid h-screen grid-rows-[auto,1fr]">
+              <TopNav />
+              <main className="overflow-y-auto">{children}</main>
+              {modal}
+            </div>
 
-          <div id="modal-root" />
-        </body>
-      </html>
+            <div id="modal-root" />
+          </body>
+        </html>
+      </CSPostHogProvider>
     </ClerkProvider>
   );
 }
